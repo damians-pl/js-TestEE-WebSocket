4432f4954a16455cd552f94f74bf0fcbdadf0198
damians-pl <damian@sulmowski.eu>
2018-11-13 19:37:07 +0100

    fix

--- agent/agent_test.js
+++ agent/agent_test.js
@@ -1,20 +1,28 @@
 const express = require('express');
 const _ = require('underscore');
-
+const request = require('sync-request');
+const exec = require('child_process').exec;
 
 const testFunctions = {
     "test_0": {
         "run": () => {
-            // Dowolny kod aby sprawdzić usługe
-            let x;
-            x = _.random(0, 10);
-
-            if(!x) return x;
-            else   return 200;
+            try {
+                var res = request('GET', 'http://googledsds.com');  
+                if( res.statusCode >= 300 ){
+                    return res.statusCode;
+                }else{
+                    return 200;
+                }
+            } catch (error) {
+                console.log( error );
+                return error;
+            }
+            
         },
         "reset": () => {
             // Dowolny kod aby wyłaczyć / restartować usługe
             console.log("Resetuje usługe 1");
+            return exec( 'sudo service httpd restart', (error, stdout, stderr) => { return true; } );
             return true;
         }
     },
